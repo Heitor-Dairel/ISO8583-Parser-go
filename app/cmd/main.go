3c00c100308dd66aa3a0322b9c9d71643433db66
Heitor-Dairel <hdgtlindo@gmail.com>
2026-06-15 17:56:03 -0300

    Refatorar o parser ISO8583: substituir o módulo processor pelo novo módulo iso e melhorar a estrutura de parsing

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/Heitor-Dairel/ISO8583-Parser-go/app/internal/processor"
+import (
+	"github.com/Heitor-Dairel/ISO8583-Parser-go/app/internal/iso"
+)
 
 func main() {
 
-	processor.ParseISO85831993("26/05/2025", "CIC1", "26/05/2025", "CIC2", "26/05/2025", "CIC3")
+	iso.NewParse()
+
+	iso.ParseMastercardIso("26/05/2025", "CIC1", "26/05/2025", "CIC2", "26/05/2025", "CIC3")
 
 }
--- app/internal/iso/iso.go
+++ app/internal/iso/iso.go
@@ -1,39 +1,58 @@
 package iso
 
 import (
+	"context"
 	"errors"
+	"log"
 
 	"github.com/Heitor-Dairel/ISO8583-Parser-go/app/internal/exception"
 	"github.com/Heitor-Dairel/ISO8583-Parser-go/app/internal/filepathiso"
 	"github.com/Heitor-Dairel/ISO8583-Parser-go/app/internal/helpers"
 	"github.com/Heitor-Dairel/ISO8583-Parser-go/app/internal/logs"
 	"github.com/Heitor-Dairel/ISO8583-Parser-go/app/internal/types"
-
-	"log"
+	"golang.org/x/sync/errgroup"
 )
 
 var fpi *filepathiso.FilePathIso = &filepathiso.FilePathIso{}
 
-func StartApp() {
+func NewParse() {
+	if err := fpi.GetPathOutput(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ParseMastercardIso(files ...string) {
 
 	var err error
+	var errg *errgroup.Group
 
-	if err = fpi.GetPathOutput(); err != nil {
-		log.Fatal(err)
+	errg, _ = errgroup.WithContext(context.Background())
+
+	for i := 0; i < len(files); i += 2 {
+
+		errg.Go(func() error {
+
+			return parseIso(files[i], files[i+1])
+
+		})
+
+	}
+
+	if err = errg.Wait(); err != nil {
+		log.Println(err)
+	}
+
+	if len(filepathiso.FilesNotFound) != 0 {
+		log.Println(filepathiso.FilesNotFound)
 	}
 
 }
 
-func NewParse() *ISO8583 {
-
-	return &ISO8583{FilePathIso: *fpi, FileDataParsed: make(types.Parse, 0, 6000)}
-
-}
-
-func (iso *ISO8583) ParseIso(fileDate, fileCycle types.File) error {
+func parseIso(fileDate, fileCycle types.File) error {
 
 	var err error
 	var logger *logs.LogIso
+	var iso *ISO8583 = &ISO8583{FilePathIso: *fpi, FileDataParsed: make(types.Parse, 0, 6000)}
 	var valid helpers.Validate = helpers.Validate{FileDate: fileDate, FileCycle: fileCycle}
 
 	if err = valid.ValidateFileInfoIso(); err != nil {

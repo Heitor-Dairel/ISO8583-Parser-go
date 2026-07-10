6a71a5ddf5514d4ae3e5237380a2e8ae8de0c0a9
Heitor-Dairel <hdgtlindo@gmail.com>
2026-07-10 11:01:39 -0300

    Adicionar carregamento de variáveis de ambiente com godotenv e atualizar a função de parsing

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Heitor-Dairel/ISO8583-Parser-go/app/internal/iso"
+	"github.com/Heitor-Dairel/ISO8583-Parser-go/app/internal/logs"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	iso.NewParse(`C:\Users\heitor.tavares\OneDrive - TRIVALE ADMINISTRACAO LTDA\Operação Processadora - Arquivos CSU`)
+	const errorMsg string = "Erro ao carregar arquivo .env."
 
-	iso.ParseISO85831993("02/06/2026", "CIC3")
+	err := godotenv.Load()
+
+	if err != nil {
+
+		logs.Loggers.Fatal(errorMsg)
+
+	}
+
+	iso.NewParse(os.Getenv("DIR_PATH"))
+
+	iso.ParseISO85831993("26/05/2025", "CIC2")
 
 }
--- go.mod
+++ go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/clipperhouse/displaywidth v0.11.0 // indirect
 	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
 	github.com/cloudwego/base64x v0.1.7 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.4.0 // indirect
 	github.com/mattn/go-runewidth v0.0.23 // indirect
